76a5ca8c36bdee4c965a4dc69bad869e665e928e
sarfaraz ahmed <71930595+sarfaraz947@users.noreply.github.com>
2022-01-16 22:18:27 +0530

    Update attendance.sql

--- attendance/attendance.sql
+++ attendance/attendance.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 26, 2021 at 03:39 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `attendance`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
@@ -36,18 +13,9 @@ CREATE TABLE `admin` (
   `lastname` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`admin_id`, `username`, `password`, `firstname`, `middlename`, `lastname`) VALUES
 (1, 'itsourcecode.co', 'itsourcecode', 'jude', 'reyes', 'suares');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student`
---
 
 CREATE TABLE `student` (
   `student_id` int(11) NOT NULL,
@@ -59,18 +27,10 @@ CREATE TABLE `student` (
   `section` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student`
---
 
 INSERT INTO `student` (`student_id`, `student_no`, `firstname`, `middlename`, `lastname`, `course`, `section`) VALUES
 
 
--- --------------------------------------------------------
-
---
--- Table structure for table `time`
---
 
 CREATE TABLE `time` (
   `time_id` int(11) NOT NULL,
@@ -80,57 +40,18 @@ CREATE TABLE `time` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `time`
---
 
 INSERT INTO `time` (`time_id`, `student_no`, `student_name`, `time`, `date`) VALUES
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `student`
---
 ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`);
-
---
--- Indexes for table `time`
---
 ALTER TABLE `time`
   ADD PRIMARY KEY (`time_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `student`
---
 ALTER TABLE `student`
   MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `time`
---
 ALTER TABLE `time`
   MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
